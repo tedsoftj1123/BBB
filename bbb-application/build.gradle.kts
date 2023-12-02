37a603670a6709fcb78381c350c3af1a3dd4d1fa
이하성 <tedsoftj1123@gmail.com>
2023-12-02 17:38:16 +0900

    clear

--- bbb-application/build.gradle.kts
+++
@@ -1,4 +0,0 @@
-dependencies {
-    testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
-    testImplementation("io.mockk:mockk:1.13.2")
-}
--- settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-rootProject.name = "BBB"
-
-include("bbb-infrastructure", "bbb-application")
