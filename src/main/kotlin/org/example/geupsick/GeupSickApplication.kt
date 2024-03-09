60f69db514bb98059db49b87ff99b6576ca038cd
이하성 <tedsoftj1123@gmail.com>
2024-03-09 22:17:20 +0900

    feat: init project

---
+++ src/main/kotlin/org/example/geupsick/GeupSickApplication.kt
@@ -0,0 +1,11 @@
+package org.example.geupsick
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GeupSickApplication
+
+fun main(args: Array<String>) {
+    runApplication<GeupSickApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/geupsick/GeupSickApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.geupsick
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GeupSickApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
